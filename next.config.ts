b7a5bcef5c33f9206a65f7d54bf71cdc4741ad33
Frederik Vanhevel <frederik.vanhevel@gmail.com>
2025-12-03 16:45:46 +0100

    fix build

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/cicero-app' : '',
+  basePath: isProd ? '/cicero-demo' : '',
   images: {
     unoptimized: true,
     remotePatterns: [
